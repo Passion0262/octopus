a7df28c64caa9e0d1da994f2e7693fe445407926
Passion0262 <209068171@qq.com>
2021-05-13 16:38:31 +0800

    connect database successfully

--- src/main/java/com/example/octopus/controller/studentController.java
+++ src/main/java/com/example/octopus/controller/studentController.java
@@ -1,4 +1,21 @@
 package com.example.octopus.controller;
 
+import com.example.octopus.entity.Student;
+import com.example.octopus.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class studentController {
+
+    @Autowired
+    StudentService studentService;
+
+    @RequestMapping("/student")
+    public Student showStudent() {
+        return studentService.findStudentById(1L);
+    }
+
 }
