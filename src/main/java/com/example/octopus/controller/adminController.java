2787f845bc87247fe6aecdd2df8419cf934ba250
xuyuxuan <84764015@qq.com>
2021-05-21 16:27:38 +0800

    move static/libs to static/js/libs

--- src/main/java/com/example/octopus/controller/adminController.java
+++ src/main/java/com/example/octopus/controller/adminController.java
@@ -4,13 +4,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
 @Controller
 public class adminController {
 
     @RequestMapping("/admin_index")
-    public String index(Model model) {
+    public String admin_index(Model model) {
         model.addAttribute("username", "李四");
         return "admin_index";
+    }
+
+    @RequestMapping("/admin_major")
+    public String admin_major(Model model) {
+        model.addAttribute("username", "李四");
+        return "admin_major";
     }
 }
