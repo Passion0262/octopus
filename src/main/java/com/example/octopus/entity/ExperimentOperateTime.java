f5bb5d3fd13cf2c423ea663d34f30f1e694531d5
GuessWhoIAm <zihao11163@163.com>
2021-05-22 13:42:28 +0800

    hao

---
+++ src/main/java/com/example/octopus/entity/ExperimentOperateTime.java
@@ -0,0 +1,41 @@
+package com.example.octopus.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+/**
+ * @author: Hao
+ * @date: 2021/5/22 13:05
+ *   实验操作时长
+ */
+@Entity
+@Data
+@Table
+public class ExperimentOperateTime {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String stuNumber;  //学生学号
+
+    private String stuName;  //学生姓名
+
+    private String className;  //班级
+
+    private String lessonPlan;  //开课计划
+
+    private String experimentMission;  //实验任务
+
+    private String operateTotalTime;  //操作总时长
+
+    private String operateFirstTime;  //首次操作时间
+
+    private String operateLastTime;  //末次操作时间
+
+    private String operateDetail;  //操作详情
+
+    private String experimentMonitor;  //实验监控
+
+
+}
