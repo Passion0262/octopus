4118c1c7823232663a14447a9d58ce9cea5e77a3
xuyuxuan <84764015@qq.com>
2021-05-24 11:42:24 +0800

    add admin pages and update table's js/css

--- src/main/java/com/example/octopus/controller/adminController.java
+++ src/main/java/com/example/octopus/controller/adminController.java
@@ -13,6 +13,18 @@ public class adminController {
         return "admin_login";
     }
 
+    @RequestMapping("/admin_userinfo")
+    public String admin_userinfo(Model model) {
+        model.addAttribute("username", "李四");
+        return "admin_userinfo";
+    }
+
+    @RequestMapping("/admin_reset_pwd")
+    public String admin_reset_pwd(Model model) {
+        model.addAttribute("username", "李四");
+        return "admin_reset_pwd";
+    }
+
     @RequestMapping("/admin_index")
     public String admin_index(Model model) {
         model.addAttribute("username", "李四");
@@ -76,7 +88,37 @@ public class adminController {
     @RequestMapping("/admin_pc_type")
     public String admin_pc_type(Model model) {
         model.addAttribute("username", "李四");
-        return "admin_video_log";
+        return "admin_pc_type";
+    }
+
+    @RequestMapping("/admin_cluster")
+    public String admin_cluster(Model model) {
+        model.addAttribute("username", "李四");
+        return "admin_cluster";
+    }
+
+    @RequestMapping("/admin_student_pc")
+    public String admin_student_pc(Model model) {
+        model.addAttribute("username", "李四");
+        return "admin_student_pc";
+    }
+
+    @RequestMapping("/admin_report_template")
+    public String admin_report_template(Model model) {
+        model.addAttribute("username", "李四");
+        return "admin_report_template";
+    }
+
+    @RequestMapping("/admin_video")
+    public String admin_video(Model model) {
+        model.addAttribute("username", "李四");
+        return "admin_video";
+    }
+
+    @RequestMapping("/admin_video_class")
+    public String admin_video_class(Model model) {
+        model.addAttribute("username", "李四");
+        return "admin_video_class";
     }
 
 }
