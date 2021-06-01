c226bdd99fd247d2131293e4c99e63dbffbd79e6
wendiudiu <1270771226@qq.com>
2021-06-01 14:08:29 +0800

    update all htmls' top bar

--- src/main/java/com/example/octopus/controller/studentController.java
+++ src/main/java/com/example/octopus/controller/studentController.java
@@ -9,6 +9,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import com.example.octopus.entity.user.Student;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class studentController {
@@ -18,35 +21,61 @@ public class studentController {
     @Autowired
     UserService userService;
 
+    @RequestMapping("/login")
+    public String login(Model model) {
+        return "auth-login";
+    }
+
     @RequestMapping("/")
-    public String showHome() {
+    public String showHome(Model model, HttpSession session) {
         String stuNumber = SecurityContextHolder.getContext().getAuthentication().getName();
-        logger.info("当前登陆用户：" + stuNumber);
-        return "index";
+        Student stu = userService.findStudentByStuNumber(stuNumber);
+        logger.info("当前登陆用户：" + stuNumber+":"+stu.getName());
+//        model.addAttribute("stu", stu);
+
+        session.setAttribute("stuname",stu.getName());
+        return "redirect:/index";
     }
 
     @RequestMapping("/index")
-    public String index(Model model) {
-        model.addAttribute("username", "李四");
+    public String index(Model model,HttpSession session) {
+        String stuname = (String)session.getAttribute("stuname");
+        model.addAttribute("stuname", stuname);
         return "index";
+    }
+
+    @RequestMapping("/userinfo")
+    public String userinfo(Model model,HttpSession session) {
+        String stuname = (String)session.getAttribute("stuname");
+        model.addAttribute("stuname", stuname);
+
+        String stuNumber = SecurityContextHolder.getContext().getAuthentication().getName();
+        Student stu = userService.findStudentByStuNumber(stuNumber);
+        model.addAttribute("stu", stu);
+
+        return "userinfo";
     }
 
 
     @RequestMapping("/applycourse")
-    public String applycourse(Model model) {
-        model.addAttribute("username", "李四");
+    public String applycourse(Model model,HttpSession session) {
+        String stuname = (String)session.getAttribute("stuname");
+        model.addAttribute("stuname", stuname);
         return "applycourse";
     }
 
 
     @RequestMapping("/apply_detail")
-    public String apply_detail(Model model) {
+    public String apply_detail(Model model,HttpSession session) {
+        String stuname = (String)session.getAttribute("stuname");
+        model.addAttribute("stuname", stuname);
         return "apply_detail";
     }
 
     @RequestMapping("/mycourse")
-    public String mycourse(Model model) {
-        model.addAttribute("username", "李四");
+    public String mycourse(Model model,HttpSession session) {
+        String stuname = (String)session.getAttribute("stuname");
+        model.addAttribute("stuname", stuname);
         return "mycourse";
     }
 
@@ -56,58 +85,61 @@ public class studentController {
     }
 
     @RequestMapping("/experiment_task")
-    public String experiment_task(Model model) {
-        model.addAttribute("username", "李四");
+    public String experiment_task(Model model,HttpSession session) {
+        String stuname = (String)session.getAttribute("stuname");
+        model.addAttribute("stuname", stuname);
         return "experiment_task";
     }
 
     @RequestMapping("/experiment_task_detail")
-    public String experiment_task_detail(Model model) {
-
+    public String experiment_task_detail(Model model,HttpSession session) {
+        String stuname = (String)session.getAttribute("stuname");
+        model.addAttribute("stuname", stuname);
         return "experiment_task_detail";
     }
 
     @RequestMapping("/projects")
-    public String projects(Model model) {
-        model.addAttribute("username", "李四");
+    public String projects(Model model,HttpSession session) {
+        String stuname = (String)session.getAttribute("stuname");
+        model.addAttribute("stuname", stuname);
         return "projects";
     }
 
     @RequestMapping("/project_detail")
-    public String project_detail(Model model) {
-
+    public String project_detail(Model model,HttpSession session) {
+        String stuname = (String)session.getAttribute("stuname");
+        model.addAttribute("stuname", stuname);
         return "project_detail";
     }
 
     @RequestMapping("/studylog")
-    public String studylog(Model model) {
-        model.addAttribute("username", "李四");
+    public String studylog(Model model,HttpSession session) {
+        String stuname = (String)session.getAttribute("stuname");
+        model.addAttribute("stuname", stuname);
         return "studylog";
     }
 
     @RequestMapping("/studylog_detail")
-    public String studylog_detail(Model model) {
-        model.addAttribute("username", "李四");
+    public String studylog_detail(Model model,HttpSession session) {
+        String stuname = (String)session.getAttribute("stuname");
+        model.addAttribute("stuname", stuname);
         return "studylog_detail";
     }
 
     @RequestMapping("/datasets")
-    public String datasets(Model model) {
-        model.addAttribute("username", "李四");
+    public String datasets(Model model,HttpSession session) {
+        String stuname = (String)session.getAttribute("stuname");
+        model.addAttribute("stuname", stuname);
         return "datasets";
     }
 
     @RequestMapping("/dataset_detail")
-    public String dataset_detail(Model model) {
-
+    public String dataset_detail(Model model,HttpSession session) {
+        String stuname = (String)session.getAttribute("stuname");
+        model.addAttribute("stuname", stuname);
         return "dataset_detail";
     }
 
-    @RequestMapping("/login")
-    public String login(Model model) {
-        model.addAttribute("username", "李四");
-        return "auth-login";
-    }
 
 
 
@@ -120,11 +152,4 @@ public class studentController {
 //        model.addAttribute("username", username);
 //        return "index";
 //    }
-
-    @RequestMapping("/userinfo")
-    public String userinfo(Model model) {
-
-        return "userinfo";
-    }
-
 }
