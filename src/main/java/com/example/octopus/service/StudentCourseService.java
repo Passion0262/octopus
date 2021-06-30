77dc30e47c60e6a3c141e04edfa98fb127d4feef
GuessWhoIAm <zihao11163@163.com>
2021-06-30 21:32:11 +0800

    fix some student_course's interfaces

--- src/main/java/com/example/octopus/service/StudentCourseService.java
+++ src/main/java/com/example/octopus/service/StudentCourseService.java
@@ -32,11 +32,18 @@ public interface StudentCourseService {
     List<Long> listCourseIdsByStuNumber(long stuNumber);
 
     /**
-     * 根据教师编号返回教授的课程的student-course list
+     * 根据教师号返回教授的课程的student-course list
      * @param teaNumber 教师编号
      * @return student-course list
      */
     List<StudentCourse> listStudentCoursesByTeaNumber(long teaNumber);
+
+    /**
+     * 根据学生号返回学生所选的student_course list
+     * @param stuNumber 学生号
+     * @return student_course list
+     */
+    List<StudentCourse> listStudentCoursesByStuNumber(long stuNumber);
 
     /**
      * 查询学生是否选某一门课
@@ -47,13 +54,18 @@ public interface StudentCourseService {
     boolean isChosen(long stuNumber, long courseId);
 
     /**
-     * 新增学生选课记录 根据学生id，课程id
+     * 根据学生id，课程id，更新或新增学生选课记录
      * @param stuNumber 学号
      * @param courseId 课程id
-     * @return 是否成功选课
+     * @return 是否成功更新或成功选课
      */
     boolean insertStudentCourse(long stuNumber, long courseId);
 
+    /**
+     * （不推荐使用）仅限新增学生选课记录
+     * @param studentCourse 学生选课实体
+     * @return 是否成功选课
+     */
     boolean insertStudentCourse(StudentCourse studentCourse);
 
     /**
--- src/test/java/com/example/octopus/OctopusApplicationTests.java
+++ src/test/java/com/example/octopus/OctopusApplicationTests.java
@@ -3,6 +3,9 @@ package com.example.octopus;
 import com.example.octopus.dao.ClassMapper;
 import com.example.octopus.dao.TeacherMapper;
 import com.example.octopus.dao.UserMapper;
+import com.example.octopus.dao.experiment.ChapterMapper;
+import com.example.octopus.dao.experiment.ModuleMapper;
+import com.example.octopus.entity.user.Major;
 import com.example.octopus.entity.user.Student;
 import com.example.octopus.entity.user.Teacher;
 import com.example.octopus.service.*;
@@ -13,6 +16,8 @@ import com.jcraft.jsch.SftpException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Map;
 
 @SpringBootTest
 class OctopusApplicationTests {
@@ -40,19 +45,23 @@ class OctopusApplicationTests {
 	UserService userService;
 	@Autowired
 	TeacherService tea;
+	@Autowired
+	MajorService majorService;
 
+	@Autowired
+	ModuleMapper moduleMapper;
+	@Autowired
+	ChapterMapper chapterMapper;
 	@Test
 	void interfaceTest() {
 
-        Teacher teacher = new Teacher(6, "LuBenwei", "lbwnb", 7, false, "33333333333", "卢本伟广场学院");
-		System.out.println(tea.updateTeacher(teacher));
+//		System.out.println(studentCourseService.insertStudentCourse(3L,1L));
+		System.out.println(studentCourseService.listStudentCoursesByStuNumber(2));
 	}
 
 
 	@Test
 	void dockerTest() throws JSchException, SftpException {
-//        System.out.println(dockerService.getDockerList());
-//        dockerService.findDockerById(111);
 		dockerService.createNewDocker(2);
 		ShellUtils shellUtils = new ShellUtils();
 
