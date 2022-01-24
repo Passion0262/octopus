5ce72cbcae1c5c5de3c4e3caac7470583033b586
xuyuxuan <84764015@qq.com>
2022-01-24 20:54:59 +0800

    Add admin_category.html to show categories.

--- src/main/java/com/example/octopus/controller/personalAdminController.java
+++ src/main/java/com/example/octopus/controller/personalAdminController.java
@@ -338,20 +338,20 @@ public class personalAdminController {
 			logger.info("管理员{}进入admin_plan_edit，获取Plan, id={}", user, id);
 			try {
 				Plan plan = planService.getPlanById(id);
-				List<String> categoryNames = Arrays.asList(plan.getCategoryNames().split(";"));
+				//List<String> categoryNames = Arrays.asList(plan.getCategoryNames().split(";"));
 //				String[] arr = new String[] {"算法工程师"};
 //				List<String> categoryNames = Arrays.asList(arr);
 				model.addAttribute("plan", plan);
-				List<Category> category_list = categoryService.listAllCategory();
-				for (int i=0; i<category_list.size(); i++){
-					if (categoryNames.contains(category_list.get(i).getName())){
-						category_list.get(i).setBrief("true");
-					}else{
-						category_list.get(i).setBrief("false");
-					}
-					System.out.println(category_list.get(i));
-				}
-				model.addAttribute("category", category_list);
+				//List<Category> category_list = categoryService.listAllCategory();
+//				for (int i=0; i<category_list.size(); i++){
+//					if (categoryNames.contains(category_list.get(i).getName())){
+//						category_list.get(i).setBrief("true");
+//					}else{
+//						category_list.get(i).setBrief("false");
+//					}
+//					System.out.println(category_list.get(i));
+//				}
+//				model.addAttribute("category", category_list);
 
 				return new ModelAndView("admin_plan_edit", "planmodel", model);
 			} catch (Exception e) {
@@ -372,8 +372,53 @@ public class personalAdminController {
 		if (role_id == 5) {
 			logger.info("管理员{}提交修改的plan: [{}]", user, plan);
 			try {
-				//planService.
+				planService.updatePlan(plan);
 				return new ModelAndView("redirect:/admin_plan");
+			} catch (Exception e) {
+				return new ModelAndView("redirect:/admin_error");
+			}
+		} else {
+			return new ModelAndView("redirect:/login");
+		}
+	}
+
+	//课程类别管理
+	@RequestMapping("/admin_category")
+	public String admin_category(HttpServletRequest request, Model model) {
+		String user_id = SecurityContextHolder.getContext().getAuthentication().getName();
+		if (!user_id.equals(cookieThings.getCookieUserNum(request, COOKIE_NAME))) return "redirect:/login";
+
+		if (!cookieCheck(model, request)) return "redirect:/login";
+
+		long user = Long.parseLong(cookieThings.getCookieUserNum(request, COOKIE_NAME));
+		int role_id = sysUserRoleService.getRoleIdByUserId(user);  // 获取角色，管理员还是教师
+
+		if (role_id == 5) {
+			try {
+				model.addAttribute("categories", categoryService.listAllCategory());
+				return "admin_category";
+			} catch (Exception e) {
+				return "redirect:/admin_error";
+			}
+		} else {
+			return "redirect:/login";
+		}
+	}
+
+	//删除课程类别
+	@RequestMapping("/admin_category_delete")
+	public ModelAndView admin_category_delete(HttpServletRequest request, Model model) {
+		if (!cookieCheck(model, request)) return new ModelAndView("redirect:/login");
+
+		long user = Long.parseLong(cookieThings.getCookieUserNum(request, COOKIE_NAME));
+		int role_id = sysUserRoleService.getRoleIdByUserId(user);  // 获取角色
+
+		if (role_id == 5) {
+			long id = Long.parseLong(request.getParameter("id"));
+			logger.info("管理员{}删除课程类别[{}]", user, id);
+			try {
+				categoryService.deleteCategoryById(id);
+				return new ModelAndView("redirect:/admin_category");
 			} catch (Exception e) {
 				return new ModelAndView("redirect:/admin_error");
 			}
